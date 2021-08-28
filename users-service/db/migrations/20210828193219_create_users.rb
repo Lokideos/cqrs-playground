eacf85c77232cacfa84772e5d1e9bdbcf44ff451
lokideos <archmage09@gmail.com>
2021-08-28 23:06:50 +0300

    Simplify user entity (it now has onlu full name)

--- users-service/db/migrations/20210828193219_create_users.rb
+++ users-service/db/migrations/20210828193219_create_users.rb
@@ -4,13 +4,11 @@ Sequel.migration do
   up do
     create_table(:users) do
       primary_key :id, type: :Bignum
-      column :name, 'character varying', null: false
-      column :email, 'citext', null: false
-      column :password_digest, 'character varying', null: false
+      column :full_name, 'character varying', null: false
       column :created_at, 'timestamp(6) without time zone', null: false
       column :updated_at, 'timestamp(6) without time zone', null: false
 
-      index [:email], name: :index_users_on_email, unique: true
+      index [:full_name], name: :index_users_on_full_name, unique: true
     end
   end
 
