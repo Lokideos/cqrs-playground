599a7105def776080621b3a8b319fcd4dceb75fc
lokideos <archmage09@gmail.com>
2021-08-29 10:42:05 +0300

    Send events about user creation

--- users-service/lib/commands_handler/create_user.rb
+++ users-service/lib/commands_handler/create_user.rb
@@ -1,6 +1,9 @@
 module CommandsHandler
   class CreateUser
-    include Import[user_repo: 'write_model.repositories.users',]
+    include Import[
+              user_repo: 'write_model.repositories.users',
+              producer: 'kafka_producer'
+            ]
 
     def call(payload)
       existing_user = user_repo.find_by_full_name(full_name: payload[:full_name])
@@ -8,7 +11,12 @@ module CommandsHandler
       if existing_user
         existing_user
       else
-        user_repo.create(payload)
+        new_user = user_repo.create(payload)
+
+        event = Events::UserCreated.new(new_user.to_h)
+        producer.call(event: event, topic: 'user-topic')
+
+        new_user
       end
     end
   end
