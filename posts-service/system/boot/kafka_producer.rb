e4cc8c8191156ac79f0e17ab041496b8b7696371
lokideos <archmage09@gmail.com>
2021-08-30 22:32:06 +0300

    Setup karafka for posts service

---
+++ posts-service/system/boot/kafka_producer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'json'
+
+App.boot(:kafka_producer) do |app|
+  init do
+    require 'waterdrop'
+
+    WaterDrop.setup do |config|
+      config.deliver = true
+      config.kafka.seed_brokers = %w[kafka://localhost:9092]
+    end
+
+    class KafkaProducer
+      def call(event:, topic:)
+        WaterDrop::SyncProducer.call(event.serialize.to_json, topic: topic)
+      end
+    end
+
+    register(:kafka_producer, KafkaProducer.new)
+  end
+end
