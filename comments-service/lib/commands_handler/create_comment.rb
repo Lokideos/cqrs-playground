aac305a74a8ebe75184091c1b8037e47b77aab34
lokideos <archmage09@gmail.com>
2021-08-30 21:16:15 +0300

    Do not create comments with empty bodies

--- comments-service/lib/commands_handler/create_comment.rb
+++ comments-service/lib/commands_handler/create_comment.rb
@@ -3,6 +3,8 @@ module CommandsHandler
     include Import[comments_repo: 'write_model.repositories.comments']
 
     def call(payload)
+      return {} if payload[:body] == ''
+
       new_comment = comments_repo.create(payload)
 
       # event = Events::CommentCreated.new(new_comment.to_h)
