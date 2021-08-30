90dac3110bd63efab1cdf98e437a0c965cc06c24
lokideos <archmage09@gmail.com>
2021-08-30 22:56:08 +0300

    Correct posts write model db schema

--- posts-service/db/write_db_migrations/20210830123322_create_posts.rb
+++ posts-service/db/write_db_migrations/20210830123322_create_posts.rb
@@ -4,7 +4,9 @@ Sequel.migration do
   up do
     create_table(:posts) do
       primary_key :id, type: :Bignum
-      column :text, 'character varying', null: false
+      column :title, 'character varying', null: false
+      column :body, 'character varying', null: false
+      column :author_id, 'bigint', null: false
       column :created_at, 'timestamp(6) without time zone', null: false
       column :updated_at, 'timestamp(6) without time zone', null: false
     end
--- posts-service/db/write_db_schema.rb
+++ posts-service/db/write_db_schema.rb
@@ -2,7 +2,9 @@ Sequel.migration do
   change do
     create_table(:posts) do
       primary_key :id, :type=>:Bignum
-      column :text, "character varying", :null=>false
+      column :title, "character varying", :null=>false
+      column :body, "character varying", :null=>false
+      column :author_id, "bigint", :null=>false
       column :created_at, "timestamp(6) without time zone", :null=>false
       column :updated_at, "timestamp(6) without time zone", :null=>false
     end
