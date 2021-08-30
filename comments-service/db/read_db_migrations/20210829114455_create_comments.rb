12d2eaa2a7fe235fad579c2ff6f4357583713c6f
lokideos <archmage09@gmail.com>
2021-08-30 20:49:20 +0300

    Fix comments data

--- comments-service/db/read_db_migrations/20210829114455_create_comments.rb
+++ comments-service/db/read_db_migrations/20210829114455_create_comments.rb
@@ -4,7 +4,9 @@ Sequel.migration do
   up do
     create_table(:comments) do
       primary_key :id, type: :Bignum
-      column :text, 'character varying', null: false
+      column :post_id, 'bigint', null: false
+      column :body, 'character varying', null: false
+      column :author_name, 'character varying', null: false
       column :created_at, 'timestamp(6) without time zone', null: false
       column :updated_at, 'timestamp(6) without time zone', null: false
     end
--- comments-service/db/write_db_schema.rb
+++ comments-service/db/write_db_schema.rb
@@ -2,7 +2,9 @@ Sequel.migration do
   change do
     create_table(:comments) do
       primary_key :id, :type=>:Bignum
-      column :text, "character varying", :null=>false
+      column :post_id, "bigint", :null=>false
+      column :body, "character varying", :null=>false
+      column :author_name, "character varying", :null=>false
       column :created_at, "timestamp(6) without time zone", :null=>false
       column :updated_at, "timestamp(6) without time zone", :null=>false
     end
