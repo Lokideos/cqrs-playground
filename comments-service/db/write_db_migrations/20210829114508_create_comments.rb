9e8ac7417da1e41e28134f79541b26244a48e13d
lokideos <archmage09@gmail.com>
2021-08-30 21:09:27 +0300

    Fix comments for write model

--- comments-service/db/write_db_migrations/20210829114508_create_comments.rb
+++ comments-service/db/write_db_migrations/20210829114508_create_comments.rb
@@ -6,7 +6,7 @@ Sequel.migration do
       primary_key :id, type: :Bignum
       column :post_id, 'bigint', null: false
       column :body, 'character varying', null: false
-      column :author_name, 'character varying', null: false
+      column :author_id, 'bigint', null: false
       column :created_at, 'timestamp(6) without time zone', null: false
       column :updated_at, 'timestamp(6) without time zone', null: false
     end
--- comments-service/lib/commands/create_comment.rb
+++ comments-service/lib/commands/create_comment.rb
@@ -1,5 +1,7 @@
 module Commands
   class CreateComment < Dry::Struct
-    attribute :text, Types::String
+    attribute :post_id, Types::Integer
+    attribute :body, Types::String
+    attribute :author_id, Types::Integer
   end
 end
