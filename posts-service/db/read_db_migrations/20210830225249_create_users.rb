ccc9d541587ce500b1d1c4522195277247ee71ff
lokideos <archmage09@gmail.com>
2021-08-30 22:53:33 +0300

    Add users to the read model db

---
+++ posts-service/db/read_db_migrations/20210830225249_create_users.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  up do
+    create_table(:users) do
+      primary_key :id, type: :Bignum
+      column :full_name, 'character varying', null: false
+      column :created_at, 'timestamp(6) without time zone', null: false
+      column :updated_at, 'timestamp(6) without time zone', null: false
+
+      index [:full_name], name: :index_users_on_full_name, unique: true
+    end
+  end
+
+  down do
+    drop_table(:users)
+  end
+end
--- posts-service/db/read_db_schema.rb
+++ posts-service/db/read_db_schema.rb
@@ -24,5 +24,14 @@ Sequel.migration do
       
       primary_key [:filename]
     end
+    
+    create_table(:users) do
+      primary_key :id, :type=>:Bignum
+      column :full_name, "character varying", :null=>false
+      column :created_at, "timestamp(6) without time zone", :null=>false
+      column :updated_at, "timestamp(6) without time zone", :null=>false
+      
+      index [:full_name], :name=>:index_users_on_full_name, :unique=>true
+    end
   end
 end
